e989ff067e9618c6ad2ab55990d581b241215811
Praktikant <praktikant@Sten-iMac.local>
2014-07-21 15:41:42 +0200

    Initial Commit

---
+++ Bubbles/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Bubbles
+//
+//  Created by Praktikant on 21.07.14.
+//  Copyright Praktikant 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Bubbles/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bubbles
+//
+//  Created by Praktikant on 21.07.14.
+//  Copyright Praktikant 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
